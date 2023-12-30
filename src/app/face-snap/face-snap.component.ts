83bb66105b6cdbbd58c9f7d35e495fd6248c4ac2
Adam <adam.mehdaouijorge@gmail.com>
2023-12-30 18:34:26 +0100

    chore: angular properties learning

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  date!: Date;
+  imageUrl!: string;
+  snaps!: number;
 
+  ngOnInit() {
+    this.title = 'Kyoto';
+    this.description = 'Paysage du Japon';
+    this.date = new Date();
+    this.imageUrl = "assets/images/kyoto.jpeg"
+    this.snaps = 5;
+  }
 }
